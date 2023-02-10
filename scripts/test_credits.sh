8f29eb6b171cb423ee572b27b5891b4c4d2ef0b4
nhpd <dmitry.klpv@gmail.com>
2023-02-10 18:16:04 +0800

    fix script

--- scripts/test_credits.sh
+++ scripts/test_credits.sh
@@ -2,12 +2,12 @@
 # test in baryon-1 testnet
 CONTRACT=../artifacts/credits.wasm
 CHAINID=baryon-1
-NEUTRON_DIR=${NEUTRON_DIR:-../neutron}
-HOME=${NEUTRON_DIR}/data/baryon-1/
+KEYS_HOME=~/.baryon-1
 NEUTROND_BIN=neutrond
-NODE=https://rpc.baryon.ntrn.info
+NODE=https://rpc.baryon.ntrn.info:443
 TEST_WALLET=baryon_testwallet
-TEST_ADDR=$(${NEUTROND_BIN} keys show ${TEST_WALLET} --keyring-backend test -a --home ${HOME})
+TEST_ADDR=$(${NEUTROND_BIN} keys show ${TEST_WALLET} --keyring-backend test -a --home ${KEYS_HOME})
+GAS_PRICES=0.0025untrn
 
 echo "Store contract"
 RES=$(${NEUTROND_BIN} tx wasm store ${CONTRACT} \
@@ -15,29 +15,29 @@ RES=$(${NEUTROND_BIN} tx wasm store ${CONTRACT} \
     --gas 50000000 \
     --chain-id ${CHAINID} \
     --broadcast-mode=block \
-    --gas-prices 0.0025stake -y \
+    --gas-prices ${GAS_PRICES}  -y \
     --output json \
     --keyring-backend test \
-    --home ${HOME} \
+    --home ${KEYS_HOME} \
     --node ${NODE})
-CREDITS_CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[0].value')
+CREDITS_CONTRACT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
 echo $RES
 echo $CREDITS_CONTRACT_CODE_ID
 
-INIT_CREDITS_CONTRACT_MSG='{"when_claimable": "1676016745597000", "dao_address": "${TEST_ADDR}", "airdrop_address": "${TEST_ADDR}", "sale_contract_address": "${TEST_ADDR}", "lockdrop_address": "${TEST_ADDR}"}'
+INIT_CREDITS_CONTRACT_MSG="{\"when_claimable\": \"1676016745597000\", \"dao_address\": \"${TEST_ADDR}\", \"airdrop_address\": \"${TEST_ADDR}\", \"sale_contract_address\": \"${TEST_ADDR}\", \"lockdrop_address\": \"${TEST_ADDR}\"}"
 
 echo "Instantiate"
 RES=$(${NEUTROND_BIN} tx wasm instantiate $CREDITS_CONTRACT_CODE_ID \
     "$INIT_CREDITS_CONTRACT_MSG" \
     --from ${TEST_ADDR} \
-    --admin ${TEST_ADDR} -y \
+    --admin ${TEST_ADDR}  -y \
     --chain-id ${CHAINID} \
     --output json \
     --broadcast-mode=block \
     --label "init" \
     --keyring-backend test \
-    --gas-prices 0.0025untrn \
-    --home ${HOME} \
+    --gas-prices ${GAS_PRICES} \
+    --home ${KEYS_HOME} \
     --node ${NODE})
 echo $RES
 CREDITS_CONTRACT_ADDRESS=$(echo $RES | jq -r '.logs[0].events[0].attributes[0].value')
@@ -51,16 +51,34 @@ RES=$(${NEUTROND_BIN} tx wasm execute $CREDITS_CONTRACT_ADDRESS \
     --chain-id ${CHAINID} \
     --output json \
     --broadcast-mode=block \
-    --gas-prices 0.0025untrn \
+    --gas-prices ${GAS_PRICES} \
     --gas 1000000 \
     --keyring-backend test \
-    --home ${HOME} \
+    --home ${KEYS_HOME} \
     --node ${NODE})
 echo $RES | jq
 
 echo "Query config"
 QUERY_MSG="{\"config\":{}}"
-RES=$(${BIN} query wasm contract-state smart ${CREDITS_CONTRACT_ADDRESS} \
+RES=$(${NEUTROND_BIN} query wasm contract-state smart ${CREDITS_CONTRACT_ADDRESS} \
+    "${QUERY_MSG}" \
+    --chain-id "$NEUTRON_CHAIN_ID" \
+    --output json \
+    --node ${NODE})
+echo "$RES" | jq
+
+echo "Query all accounts"
+QUERY_MSG="{\"all_accounts\":{}}"
+RES=$(${NEUTROND_BIN} query wasm contract-state smart ${CREDITS_CONTRACT_ADDRESS} \
+    "${QUERY_MSG}" \
+    --chain-id "$NEUTRON_CHAIN_ID" \
+    --output json \
+    --node ${NODE})
+echo "$RES" | jq
+
+echo "Query balance"
+QUERY_MSG="{\"balance\":{\"address\": \"${TEST_ADDR}\"}}"
+RES=$(${NEUTROND_BIN} query wasm contract-state smart ${CREDITS_CONTRACT_ADDRESS} \
     "${QUERY_MSG}" \
     --chain-id "$NEUTRON_CHAIN_ID" \
     --output json \
