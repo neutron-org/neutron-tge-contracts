85c1158004d7102d1b0783933ca0ed3fe94093e7
quasisamurai <quazisamurai@gmail.com>
2023-02-12 19:27:40 +0400

    contract & packages

---
+++ contracts/oracle/src/mock_querier.rs
@@ -0,0 +1,128 @@
+use astroport::asset::{Asset, AssetInfo, PairInfo};
+use astroport::factory::PairType;
+use astroport::factory::QueryMsg::Pair;
+use astroport::pair::CumulativePricesResponse;
+use astroport::pair::QueryMsg::CumulativePrices;
+use cosmwasm_std::testing::{MockApi, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR};
+use cosmwasm_std::{
+    from_binary, from_slice, to_binary, Addr, Coin, Empty, OwnedDeps, Querier, QuerierResult,
+    QueryRequest, SystemError, SystemResult, Uint128, WasmQuery,
+};
+use std::collections::HashMap;
+
+pub fn mock_dependencies(
+    contract_balance: &[Coin],
+) -> OwnedDeps<MockStorage, MockApi, WasmMockQuerier> {
+    let custom_querier: WasmMockQuerier =
+        WasmMockQuerier::new(MockQuerier::new(&[(MOCK_CONTRACT_ADDR, contract_balance)]));
+
+    OwnedDeps {
+        storage: MockStorage::default(),
+        api: MockApi::default(),
+        querier: custom_querier,
+        custom_query_type: Default::default(),
+    }
+}
+
+pub struct WasmMockQuerier {
+    base: MockQuerier<Empty>,
+    token_querier: TokenQuerier,
+}
+
+#[derive(Clone, Default)]
+pub struct TokenQuerier {
+    // this lets us iterate over all pairs that match the first string
+    pairs: HashMap<String, CumulativePricesResponse>,
+}
+
+impl TokenQuerier {
+    pub fn set(
+        &mut self,
+        pair: Addr,
+        assets: Vec<Asset>,
+        total: Uint128,
+        cumulative_prices: Vec<(AssetInfo, AssetInfo, Uint128)>,
+    ) {
+        self.pairs = HashMap::new();
+        self.pairs.insert(
+            pair.to_string(),
+            CumulativePricesResponse {
+                assets,
+                total_share: total,
+                cumulative_prices,
+            },
+        );
+    }
+}
+
+impl Querier for WasmMockQuerier {
+    fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
+        // MockQuerier doesn't support Custom, so we ignore it completely here
+        let request: QueryRequest<Empty> = match from_slice(bin_request) {
+            Ok(v) => v,
+            Err(e) => {
+                return SystemResult::Err(SystemError::InvalidRequest {
+                    error: format!("Parsing query request: {}", e),
+                    request: bin_request.into(),
+                })
+            }
+        };
+        self.handle_query(&request)
+    }
+}
+
+impl WasmMockQuerier {
+    pub fn handle_query(&self, request: &QueryRequest<Empty>) -> QuerierResult {
+        match &request {
+            QueryRequest::Wasm(WasmQuery::Smart { contract_addr, msg }) => {
+                if contract_addr == "factory" {
+                    match from_binary(&msg).unwrap() {
+                        Pair { asset_infos } => SystemResult::Ok(
+                            to_binary(&PairInfo {
+                                asset_infos,
+                                contract_addr: Addr::unchecked("pair"),
+                                liquidity_token: Addr::unchecked("lp_token"),
+                                pair_type: PairType::Xyk {},
+                            })
+                            .into(),
+                        ),
+                        _ => panic!("DO NOT ENTER HERE"),
+                    }
+                } else {
+                    match from_binary(&msg).unwrap() {
+                        CumulativePrices { .. } => {
+                            let balance = match self.token_querier.pairs.get(contract_addr) {
+                                Some(v) => v,
+                                None => {
+                                    return SystemResult::Err(SystemError::Unknown {});
+                                }
+                            };
+                            SystemResult::Ok(to_binary(&balance).into())
+                        }
+                        _ => panic!("DO NOT ENTER HERE"),
+                    }
+                }
+            }
+            _ => self.base.handle_query(request),
+        }
+    }
+}
+impl WasmMockQuerier {
+    pub fn new(base: MockQuerier<Empty>) -> Self {
+        WasmMockQuerier {
+            base,
+            token_querier: TokenQuerier::default(),
+        }
+    }
+
+    pub fn set_cumulative_price(
+        &mut self,
+        pair: Addr,
+        assert: Vec<Asset>,
+        total: Uint128,
+        cumulative_prices: Vec<(AssetInfo, AssetInfo, Uint128)>,
+    ) {
+        self.token_querier
+            .set(pair, assert, total, cumulative_prices)
+    }
+}
---
+++ contracts/oracle/src/state.rs
@@ -0,0 +1,37 @@
+use cosmwasm_schema::cw_serde;
+
+use astroport::asset::{AssetInfo, PairInfo};
+use cosmwasm_std::{Addr, Decimal256, Uint128};
+use cw_storage_plus::{Item, SnapshotItem, Strategy};
+
+/// Stores the contract config at the given key
+pub const CONFIG: Item<Config> = Item::new("config");
+/// Stores the latest cumulative and average prices at the given key
+/// TODO: proper checkpoints
+pub const PRICE_LAST: SnapshotItem<PriceCumulativeLast> = SnapshotItem::new("price_last", "", "", Strategy::EveryBlock);
+
+/// This structure stores the latest cumulative and average token prices for the target pool
+#[cw_serde]
+pub struct PriceCumulativeLast {
+    /// The vector contains last cumulative prices for each pair of assets in the pool
+    pub cumulative_prices: Vec<(AssetInfo, AssetInfo, Uint128)>,
+    /// The vector contains average prices for each pair of assets in the pool
+    pub average_prices: Vec<(AssetInfo, AssetInfo, Decimal256)>,
+    /// The last timestamp block in pool
+    pub block_timestamp_last: u64,
+}
+
+/// Global configuration for the contract
+#[cw_serde]
+pub struct Config {
+    /// The address that's allowed to change contract parameters
+    pub owner: Addr,
+    /// The factory contract address
+    pub factory: Addr,
+    /// The assets in the pool. Each asset is described using a [`AssetInfo`]
+    pub asset_infos: Vec<AssetInfo>,
+    /// Information about the pair (LP token address, pair type etc)
+    pub pair: PairInfo,
+    /// Time between two consecutive TWAP updates.
+    pub period: u64,
+}
